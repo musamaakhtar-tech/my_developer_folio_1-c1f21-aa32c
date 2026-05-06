df6f6d1d38d6645026a9cb75a1d7008f651b8d1d
M. Usama Akhtar <musamaakhtar63@gmail.com>
2026-05-07 02:09:05 +0500

    Fix footer emoji rendering in JSX

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-  {emoji("Made with ❤️ by ")} (
+  {emoji("Made with ❤️ by ")}
   <a
     href="https://github.com/musamaakhtar-tech"
     target="_blank"
@@ -18,7 +18,6 @@ export default function Footer() {
   >
     MUA Tech
   </a>
-  )
 </p>
       </div>
     </Fade>
